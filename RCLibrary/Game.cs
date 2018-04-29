a3f4d7d7d493706814f1e72533184894555d0a1e
Mark Dickinson <marksamdickinson@gmail.com>
2018-04-28 22:50:11 -0700

    Cleaned out RCWeb. Added Three.Js and associated files for basic loading and camera controls. Added Input Buttons with images

--- RCLibrary/Game.cs
+++ RCLibrary/Game.cs
@@ -15,6 +15,10 @@ namespace RCLibrary
         List<Command> commands = new List<Command>();
         List<BuildAction> buildStright = new List<BuildAction>() { new BuildAction(TrackType.Stright), new BuildAction(TrackType.Stright), new BuildAction(TrackType.Stright) };
 
+        public RollerCoasterMaker()
+        {
+            Reset();
+        }
 
         public void Reset()
         {
